b8baef3525325fb8c5ca791b2605f94ff1e60b78
Phuc15126 <phuc31427@gmail.com>
2025-01-17 20:10:41 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,8 @@
+const nameGirl = 'Ngọc Hồng';
+const giftUrl = 'https://www.facebook.com/ngoc.hong.354375';
+const eventName = '💘 Happy BirthDay 💘';
+const titleCard = ' Tặng em bé iu của anh. Mong em thích nó '; 
+const contentCard = 'Em bé yêu ơi có điều gì làm bé phiền lòng hong ạ, bé đừng ngần ngại chia sẻ với anh nhaa. Anh biết đôi khi anh có chút chưa tốt nhưng em sẽ luôn là người mà anh cố gắng để trở nên phiên bản tuỵệt vời hơn của chính mình. Dẫu có chuyện gì anh mong chúng ta sẽ thông cảm và cùng nhau vượt qua, cùng nhau phát triển tình yêu chúng mình nhaa. Anh chúc em bé của anh có một ngày sinh nhật thật ngọt ngào và ấm áp nè, sẽ mãi luôn vui vẻ và tươi cười lên nheee. Dù chỉ là bức thư chúc mừng sinh nhật bằng code nhưng anh muốn nói là : ❤️❤️❤️Anh yêuu emmm❤️❤️❤️';
+const giftImage = 'avt.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
